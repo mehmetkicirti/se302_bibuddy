739653bc8dfd71a581c8f20ea5f68c853325f38f
mehmet <mehmetkicirti@hotmail.com>
2019-11-27 22:32:50 +0300

    Some unnecessary libraries has been deleted

--- BiBuddy.Entities/Concrete/manual.cs
+++ BiBuddy.Entities/Concrete/manual.cs
@@ -1,11 +1,7 @@
 ﻿using BiBuddy.Entities.Abstract;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BiBuddy.Entities.Concrete
 {
