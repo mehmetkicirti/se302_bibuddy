4c22298dd13dba26b912bdad5728ba05e3641861
asilackux <57302005+asilackux@users.noreply.github.com>
2019-11-27 20:47:15 +0300

    GUI Part

---
+++ BiBuddy.UI/AddPanels.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BiBuddy.UI
+{
+    /// <summary>
+    /// Interaction logic for AddPanels.xaml
+    /// </summary>
+    public partial class AddPanels : Window
+    {
+        public AddPanels()
+        {
+            InitializeComponent();
+        }
+
+        private void addClickP(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // This command should be the latest command end of the line! -Asil
+        }// when the panels finish i need to collapse all of the panels. - Asil // Please use if conditions cause when the something is already collapsed and u'll try to collapse it again maybe it creates crush for this app.
+    }
+}
--- BiBuddy.UI/MainWindow.xaml.cs
+++ BiBuddy.UI/MainWindow.xaml.cs
@@ -23,6 +23,70 @@ namespace BiBuddy.UI
         public MainWindow()
         {
             InitializeComponent();
+            help.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum eros ut lorem dapibus pharetra. Etiam sit amet lectus dapibus, " +
+                "congue augue sed, scelerisque diam. Aliquam pharetra risus mauris, non convallis orci placerat sodales. Cras quis urna vitae magna varius ornare. Praesent " +
+                "aliquet luctus magna, id aliquam purus pretium ut. Etiam laoreet orci quis risus varius, non volutpat quam gravida. Vestibulum sed ultricies ex, at gravida " +
+                "augue. Donec commodo ullamcorper tempus. Ut quis lacinia nunc. Fusce consectetur aliquam lectus at maximus. Integer pretium ultricies placerat. Quisque risus " +
+                "magna, cursus sed ultricies vel, convallis nec risus. Integer consectetur sem vitae enim scelerisque convallis. Donec nulla nisl, pulvinar eget sodales et, " +
+                "accumsan sed mauris. Vivamus et turpis tortor. Mauris vitae nulla suscipit, imperdiet tellus et, pellentesque massa. Suspendisse at fringilla urna. " +
+                "Pellentesque non risus ligula. Quisque faucibus urna sed eros pulvinar, hendrerit finibus elit tempus. Morbi dignissim, urna et iaculis rhoncus, ipsum mi " +
+                "laoreet neque, posuere bibendum arcu augue sit amet arcu.";
+        }
+
+        private void searchBib(object sender, RoutedEventArgs e)
+        {
+            d1.Visibility = Visibility.Collapsed;
+            s1.Visibility = Visibility.Visible;
+        }
+
+        private void BackClickAN(object sender, RoutedEventArgs e)
+        {
+            d1.Visibility = Visibility.Visible;
+            s1.Visibility = Visibility.Collapsed;
+        }
+
+        private void HelpClick(object sender, RoutedEventArgs e)
+        {
+            d1.Visibility = Visibility.Collapsed;
+            s2.Visibility = Visibility.Visible;
+        }
+
+        private void BackClickH(object sender, RoutedEventArgs e)
+        {
+            d1.Visibility = Visibility.Visible;
+            s2.Visibility = Visibility.Collapsed;
+        }
+
+        private void ArticleClick(object sender, RoutedEventArgs e)
+        {
+            AddPanels addP = new AddPanels();
+            addP.Show();
+            addP.sPanelArticleTxB.Visibility = Visibility.Visible;
+            addP.sPanelArticleTBx.Visibility = Visibility.Visible;
+        }
+
+        private void ConferenceClick(object sender, RoutedEventArgs e)
+        {
+            AddPanels addP = new AddPanels();
+            addP.Show();
+            addP.sPanelConferenceTxB.Visibility = Visibility.Visible;
+            addP.sPanelConferenceTBx.Visibility = Visibility.Visible;
+        }
+
+        private void BookletClick(object sender, RoutedEventArgs e)
+        {
+            AddPanels addP = new AddPanels();
+            addP.Show();
+            addP.sPanelBookletTxB.Visibility = Visibility.Visible;
+            addP.sPanelBookletTBx.Visibility = Visibility.Visible;
+        }
+
+        private void BookClick(object sender, RoutedEventArgs e)
+        {
+            AddPanels addP = new AddPanels();
+            addP.Show();
+            addP.sPanelBookTxB.Visibility = Visibility.Visible;
+            addP.sPanelBookTBx.Visibility = Visibility.Visible;
         }
     }
 }
