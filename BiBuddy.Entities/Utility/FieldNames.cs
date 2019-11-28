e67c59efce68d7ddfe70fb16386463a4c324486a
mehmet <mehmetkicirti@hotmail.com>
2019-11-29 00:13:16 +0300

    To Save From Magic String. I injected into Field Names.

---
+++ BiBuddy.Entities/Utility/FieldNames.cs
@@ -0,0 +1,27 @@
+﻿namespace BiBuddy.Entities.Utility
+{
+    public class FieldNames
+    {
+        #region Fields
+        public const string Author = "Yazar";
+        public const string Title = "Başlık";
+        public const string Journal = "Dergi";
+        public const string Year = "Yıl";
+        public const string Number = "Numara";
+        public const string Volume = "Cilt";
+        public const string Pages = "Sayfalar";
+        public const string Month = "Ay";
+        public const string Note = "Not";
+        public const string Series = "Grup";
+        public const string Address = "Adres";
+        public const string Edition = "Baskı";
+        public const string ISBN = "ISBN";
+        public const string HowPublished = "NasılYayınlandı";
+        public const string BookTitle = "Kitap Başlığı";
+        public const string Editor = "Editor";
+        public const string Organization = "Organizasyon";
+        public const string Publisher = "Yayımcı";
+        public const string Chapter = "Bölüm"; 
+        #endregion
+    }
+}
