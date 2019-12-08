c9d4fd43f23da8cc9ad96fe60f9e94eb3e3283f5
mehmet <mehmetkicirti@hotmail.com>
2019-12-09 02:43:52 +0300

    Tested Sqlite Connection has been succeed

---
+++ Bibuddy.DataAccess.Test/EF/EntityFrameworkTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Bibuddy.DataAccess.Concrete;
+using Bibuddy.DataAccess.DatabaseContext.Sqlite;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bibuddy.DataAccess.Test.EF
+{
+    [TestClass]
+    public class EntityFrameworkTest
+    {
+        [TestMethod]
+        public void Get_list_all_articles_return()
+        {
+            ArticleDal articleDal = new ArticleDal(new SqliteContext());
+            var result = articleDal.GetAll();
+            //expected=> beklenen 
+            Assert.AreEqual(1,result.Count);
+        }
+
+        [TestMethod]
+        public void Get_All_with_parameter()
+        {
+            ArticleDal articleDal= new ArticleDal(new SqliteContext());
+            var result = articleDal.GetAll(x => x.author.Contains("D"));
+            Assert.AreEqual(1,result.Count);
+        }
+    }
+}
---
+++ Bibuddy.DataAccess.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bibuddy.DataAccess.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bibuddy.DataAccess.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c34e7b68-6872-4b74-ad1a-1c8f241d558a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
