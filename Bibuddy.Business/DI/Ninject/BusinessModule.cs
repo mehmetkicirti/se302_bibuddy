1b92afd428467b7f11f986589dd39f4f27f53c4a
mehmet <mehmetkicirti@hotmail.com>
2019-12-09 02:46:50 +0300

    Being Solid D word DI as injected with Ninject

---
+++ Bibuddy.Business/DI/Ninject/BusinessModule.cs
@@ -0,0 +1,33 @@
+﻿
+using Bibuddy.Business.Abstract;
+using Bibuddy.Business.Concrete;
+using Bibuddy.DataAccess.Abstract;
+using Bibuddy.DataAccess.Concrete;
+using Bibuddy.DataAccess.DatabaseContext.Sqlite;
+using Ninject.Modules;
+
+namespace Bibuddy.Business.DI.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            #region DAL
+            Bind<IArticleDal>().To<ArticleDal>().InSingletonScope();
+            Bind<IBookDal>().To<BookDal>().InSingletonScope();
+            Bind<IBookletDal>().To<BookletDal>().InSingletonScope();
+            Bind<IConferenceDal>().To<ConferenceDal>().InSingletonScope();
+            Bind<IInbookDal>().To<InBookDal>().InSingletonScope();
+            Bind<IManualDal>().To<ManualDal>().InSingletonScope();
+            Bind<IIncollectionDal>().To<InCollectionDal>().InSingletonScope();
+            #endregion
+
+            #region Manager
+
+            Bind<IUnitOfWork>().To<UnitOfWork<SqliteContext>>();
+            Bind<IArticleService>().To<ArticleManager>();
+
+            #endregion
+        }
+    }
+}
---
+++ Bibuddy.Business/DI/Ninject/InstanceFactory.cs
@@ -0,0 +1,14 @@
+﻿
+using Ninject;
+
+namespace Bibuddy.Business.DI.Ninject
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.Get<T>();
+        }
+    }
+}
