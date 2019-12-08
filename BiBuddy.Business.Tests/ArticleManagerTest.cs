1760ad1f46981d18cfc5989a8926dff1783ab2be
mehmet <mehmetkicirti@hotmail.com>
2019-12-09 02:48:26 +0300

    ArticleManager validate has been checked.

---
+++ BiBuddy.Business.Tests/ArticleManagerTest.cs
@@ -0,0 +1,31 @@
+﻿using Bibuddy.Business.Concrete;
+using Bibuddy.DataAccess.Abstract;
+using Bibuddy.DataAccess.DatabaseContext.Sqlite;
+using BiBuddy.Entities.Concrete;
+using FluentValidation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BiBuddy.Business.Tests
+{
+    [TestClass]
+    public class ArticleManagerTest
+    {
+        //BeklenenException => Validation Hatası olması gerektigini söyledik.
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Article_Either_valid_or_not_check()
+        {
+            Mock<IArticleDal> mock = new Mock<IArticleDal>();
+            ArticleManager articleManager = new ArticleManager(mock.Object,new UnitOfWork<SqliteContext>(new SqliteContext()));
+            articleManager.Add(new article()
+            {
+                author = "Deneme",
+                title = "Dneeme",
+                journal = "Dneme",
+                year = 2020,
+                month = 13
+            });
+        }
+    }
+}
---
+++ BiBuddy.Business.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BiBuddy.Business.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BiBuddy.Business.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c63313e7-e3e4-4fec-8f9e-62d6907b618f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
