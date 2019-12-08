1a443647f33463320334dc9e61b5206701a8d804
mehmet <mehmetkicirti@hotmail.com>
2019-12-09 02:53:31 +0300

    Base repository has been created.

---
+++ Bibuddy.Core/DataAccess/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BiBuddy.Entities.Abstract;
+
+namespace BiBuddy.Core.DataAccess
+{
+    //add limitation =>
+    //Here class restriction used to reach from only reference type
+    //new() => abstract and interface are a reference type ,so we are seperated from that.
+    //IEntity => each classes can be able to have some specific for standard. 
+    public interface IRepository<TEntity>
+        where TEntity : class,IEntity,new()
+    {
+        void Add(TEntity entity);
+        void Delete(int ID);
+        void Update(TEntity entity);
+        TEntity GetByID(int ID);
+        TEntity Get(Expression<Func<TEntity,bool>> filter);
+        List<TEntity> GetAll(Expression<Func<TEntity,bool>> filter = null);
+    }
+}
