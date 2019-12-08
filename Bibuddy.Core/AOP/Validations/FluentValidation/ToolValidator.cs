0f65884bbd342bde26d21a4fcc178ff9f86ffea6
mehmet <mehmetkicirti@hotmail.com>
2019-12-09 02:52:57 +0300

    Aspect of Validation to reach from each table  was created.

---
+++ Bibuddy.Core/AOP/Validations/FluentValidation/ToolValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibuddy.Core.AOP.Validations.FluentValidation
+{
+    public class ToolValidator
+    {
+        /*Why We use AOP => 
+            it will provide;
+                - reusability
+                - maintainability
+                - modularity of the projects
+        */  
+        public static void FluentValidate(IValidator validator, object obj)
+        {
+            var result = validator.Validate(obj);
+            if (result.Errors.Count>0)
+                throw new ValidationException(result.Errors);
+        }
+    }
+}
