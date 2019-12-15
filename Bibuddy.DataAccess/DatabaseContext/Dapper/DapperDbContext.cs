f33a9f6ee0bc5d0fa236b2e73b9ecacef9a9fb6f
mehmet <mehmetkicirti@hotmail.com>
2019-12-15 21:42:17 +0300

    Dapper Orm has been added connection

---
+++ Bibuddy.DataAccess/DatabaseContext/Dapper/DapperDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibuddy.DataAccess.DatabaseContext.Dapper
+{
+    public class DapperDbContext
+    {
+        private static IDbConnection _dbConnection;
+        private static string con = Path.GetFullPath("se302_bibuddy");
+
+        private static string[] path = con.Split(new string[] { "bin" }, StringSplitOptions.None);
+        //private static string dbName = @"BiBuddyDB.db";
+        private static string fullPath = String.Format(@"Data Source={0}BiBuddyDB.db", path[0]);
+        public DapperDbContext()
+        {
+            _dbConnection = new SQLiteConnection(fullPath);
+        }
+        public static IDbConnection GetDbConnection()
+        {
+            if (_dbConnection == null)
+            {
+                _dbConnection = new SQLiteConnection(fullPath);
+                return _dbConnection;
+            }
+            return _dbConnection;
+        }
+    }
+}
