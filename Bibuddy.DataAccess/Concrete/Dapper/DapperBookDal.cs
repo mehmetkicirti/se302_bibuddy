17a8a6c72f18de7318cf27dee12211f660371123
onatcakr <onatcakr@gmail.com>
2019-12-15 23:32:02 +0300

    sqlite methods created for dataaccess layer

---
+++ Bibuddy.DataAccess/Concrete/Dapper/DapperBookDal.cs
@@ -0,0 +1,59 @@
+﻿using Bibuddy.DataAccess.Abstract;
+using BiBuddy.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibuddy.DataAccess.Concrete.Dapper
+{
+    public class DapperBookDal : IBookDal
+    {
+        public void Add(book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public book Get(Expression<Func<book, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<book> GetAll(Expression<Func<book, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<book> GetAllByAuthorOrTitleIfNotExist(string author = null, string title = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<book> GetAllByYear(int? year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public book GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(book entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Bibuddy.DataAccess/Concrete/Dapper/DapperBookletDal.cs
@@ -0,0 +1,59 @@
+﻿using Bibuddy.DataAccess.Abstract;
+using BiBuddy.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibuddy.DataAccess.Concrete.Dapper
+{
+    public class DapperBookletDal : IBookletDal
+    {
+        public void Add(booklet entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public booklet Get(Expression<Func<booklet, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<booklet> GetAll(Expression<Func<booklet, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<booklet> GetAllByAuthorOrTitleIfNotExist(string author = null, string title = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<booklet> GetAllByYear(int? year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public booklet GetByID(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(booklet entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
