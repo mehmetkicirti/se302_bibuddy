4d423c1f735324a976df23323f2d2e1a9a98bb17
cemlc <crazyfr35@gmail.com>
2019-12-19 01:40:40 +0300

    Size fixed

--- BiBuddy.UI/MainWindow.xaml.cs
+++ BiBuddy.UI/MainWindow.xaml.cs
@@ -38,7 +38,6 @@ namespace BiBuddy.UI
         {
             _articleService = InstanceFactory.GetInstance<DapperArticleDal>();
             _bookService = InstanceFactory.GetInstance<DapperBookDal>();
-            DataGridMain.ItemsSource = _articleService.GetAllByAuthorOrTitleIfNotExist();
             InitializeComponent();
             
         }
