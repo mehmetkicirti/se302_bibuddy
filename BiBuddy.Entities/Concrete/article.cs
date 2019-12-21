766daa71091ff118931772965154b47532a61467
mehmet <mehmetkicirti@hotmail.com>
2019-12-21 07:56:39 +0300

    Article validation has been completed but another not yet.

--- BiBuddy.Entities/Concrete/article.cs
+++ BiBuddy.Entities/Concrete/article.cs
@@ -1,17 +1,97 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Bibuddy.Entities.ValidationRules.FluentValidation;
 using BiBuddy.Entities.Abstract;
 
 namespace BiBuddy.Entities.Concrete
 {
-    public class article:BaseEntity,IEntity
+    public class article:BaseEntity,IEntity,INotifyPropertyChanged,IDataErrorInfo
     {
+        private readonly ArticleValidate _articleValidator;
+        private string _author;
+        private string _title;
+        private string _journal;
+        public article()
+        {
+            _articleValidator = new ArticleValidate();
+        }
         //Why we do not use DataAnnatotions => because it is a contradiction for Solid Principle S
-        public string author{ get; set; }
-        public string title { get; set; }
-        public string journal { get; set; }
-        public int? number { get; set; }
+        public string author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+                OnPropertyChanged("author");
+            }
+        }
+        public string title 
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("title");
+            }
+        }
+        public string journal 
+        {
+            get
+            {
+                return _journal;
+            }
+            set
+            {
+                _journal = value;
+                OnPropertyChanged("journal");
+            }
+        }
         public int? volume { get; set; }
+        public int? number { get; set; }
         public string pages { get; set; }
         public string doi{ get; set; }
+
+        public string Error 
+        {
+            get
+            {
+                if (_articleValidator!=null)
+                {
+                    var results = _articleValidator.Validate(this);
+                    if (results!=null && results.Errors.Any())
+                    {
+                        var errors = string.Join(Environment.NewLine, results.Errors.Select(e => e.ErrorMessage).ToArray());
+                        return errors;
+                    }
+                }
+                return string.Empty;
+            }
+        } 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string this[string columnName] 
+        {
+            get
+            {
+                var firstError = _articleValidator.Validate(this).Errors.FirstOrDefault(e => e.PropertyName == columnName);
+                if (firstError!=null)
+                    return _articleValidator!=null ? firstError.ErrorMessage:"";
+                return "";
+            }
+        }
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ BiBuddy.Entities/ValidationRules/FluentValidation/ArticleValidate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using BiBuddy.Entities.Concrete;
+using BiBuddy.Entities.ValidationRules;
+using FluentValidation;
+
+namespace Bibuddy.Entities.ValidationRules.FluentValidation
+{
+    public class ArticleValidate:AbstractValidator<article>
+    {
+        public ArticleValidate()
+        {
+            #region Author
+            RuleFor(a => a.author).NotEmpty().WithMessage(errorMessage: ErrorMessages.AuthorIsRequired);
+            RuleFor(a => a.author).MinimumLength(2).WithMessage(errorMessage: ErrorMessages.AuthorMinLength);
+            RuleFor(a => a.author).MaximumLength(75).WithMessage(errorMessage: ErrorMessages.AuthorMaxLength);
+            #endregion
+           
+            #region Title
+            RuleFor(a => a.title).NotEmpty().WithMessage(errorMessage:ErrorMessages.TitleIsRequired);
+            RuleFor(a => a.title).MinimumLength(2).WithMessage(errorMessage: ErrorMessages.TitleMinLength);
+            RuleFor(a => a.title).MaximumLength(250).WithMessage(errorMessage: ErrorMessages.TitleMaxLength);
+            #endregion
+
+            #region Journal
+            RuleFor(a => a.journal).NotEmpty().WithMessage(errorMessage: ErrorMessages.JournalIsRequired);
+            RuleFor(a => a.journal).MinimumLength(2).WithMessage(errorMessage: ErrorMessages.JournalMinLength);
+            RuleFor(a => a.journal).MaximumLength(250).WithMessage(errorMessage: ErrorMessages.JournalMaxLength);
+            #endregion
+            
+            #region Year
+            RuleFor(a => a.year).Must(YearRange).WithMessage(errorMessage: ErrorMessages.YearRange);
+            #endregion
+
+            #region Month
+
+            RuleFor(a => a.month).Must(MonthRange).WithMessage(errorMessage:ErrorMessages.MonthRange);
+            
+
+            #endregion
+        }
+        private static bool MonthRange(int? arg)
+        {
+            if (arg.HasValue)
+            {
+                if (arg.Value<DateTime.MinValue.Month || arg.Value>DateTime.MaxValue.Month)
+                    return false;
+                return true;
+            }
+            else
+                return true;
+        }
+
+        private static bool YearRange(int? arg)
+        {
+            if (arg.HasValue)
+            {
+                if (arg.Value < DateTime.MinValue.Year || arg.Value > DateTime.MaxValue.Year)
+                    return false;
+                return true;
+            }
+            else
+                return true;
+        }
+    }
+}
